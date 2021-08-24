67c95f523a2c361df2d720043f3b81b7b6dad21e
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-08-24 03:19:53 +0300

    add model entities

---
+++ FirstBank/Models/Member.cs
@@ -0,0 +1,11 @@
+namespace FirstBank.Models
+{
+  public class Member
+  {
+    public int MemberId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public int PhoneNumber { get; set; }
+  }
+}
